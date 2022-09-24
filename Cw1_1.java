d5d011acba8358c23f1d3a8a174914910af8973d
Daria Gudemchuk <91084561+darinagdm@users.noreply.github.com>
2022-09-24 12:53:56 +0300

    Classwork 1. Hudemchuk Daria

---
+++ Cw1_1.java
@@ -0,0 +1,15 @@
+public class Cw1_1 {
+    private int temp1;
+    private char temp2;
+    private String temp3;
+
+    public static void main(String args[]) {
+        new Cw1_1().display();
+    }
+    
+    void display(){
+        System.out.println(temp1);
+        System.out.println(temp2);
+        System.out.println(temp3);
+    }
+}
