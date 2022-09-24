5928ed4d79f3c6069c5143759f6d185a70b36e25
Daria Gudemchuk <91084561+darinagdm@users.noreply.github.com>
2022-09-24 22:52:46 +0300

    Classwork 3
    Classwork 3. Hudemchuk Daria

---
+++ Cw2_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Represents a Cw2_1
+ * @author Daria Hudemchuk
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class Cw2_1 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your surname: ");
+        String myString = input.next();
+        System.out.print("Hello, " + myString);
+    }
+}
---
+++ Cw3_3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Cw3_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введіть ім'я: \n");
+        String name;
+        name = sc.nextLine();
+        System.out.println("Привіт, " + name + "!");
+    }
+}
